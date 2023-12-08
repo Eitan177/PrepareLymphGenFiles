d10fd1fbf2adc80fa0c81f4f32de466bdc57697e
Eitan Halper-Stromberg <Eitan177@gmail.com>
2023-12-08 14:11:07 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
-#from Bio import Entrez
+from Bio import Entrez
 #import ssl
 #import urllib.request
 import streamlit as st
 import pandas as pd
 import numpy as np
+import re
 st.title("Get flat file, sample file, and entrez list for lymphgen")
 fileuse=st.file_uploader("Upload your file", type="txt",accept_multiple_files=True)
-vaf = st.number_input("Enter VAF cutoff", min_value=0, max_value=100, value=20, step=1)
+vaf = st.number_input("Enter VAF cutoff", min_value=0, max_value=100, value=8, step=1)
 # Create an SSL context that doesn't verify the certificates
 
 #ssl._create_default_https_context = ssl._create_unverified_context
@@ -36,15 +37,21 @@ if fileuse != []:
                 f_use.drop(index, inplace=True)
             #if row['p.CHANGE'] == 'p.?':
             #    f_use.drop(index, inplace=True)
-            elif row['p.CHANGE'] == '':
-                f_use.drop(index, inplace=True)
+            #elif row['p.CHANGE'] == '':
+            #    f_use.drop(index, inplace=True)
             elif row['EFFECT'] == 'structural_interaction_variant':
                 f_use.drop(index, inplace=True)   
+            elif re.findall('rs',str(row['rsID'])) and not re.findall('CO',str(row['COSMIC'])):
+                f_use.drop(index, inplace=True)                
+            #elif row['VARIANT_TYPE'] == 'SNP':
+            #    f_use.drop(index, inplace=True)
+            elif row['FILTER'] != 'PASS':   
+                f_use.drop(index, inplace=True) 
         f_use=get_entrez_id(f_use)
         f_use['Sample']=f_use['#SAMPLE_NAME'].astype(str)
         f_use['ENTREZ.ID']=f_use['entrezgene_id']
         f_use['Type']=''
-        f_use['Type'][f_use['EFFECT'].str.contains('syn')]='Synon'
+        f_use['Type'][f_use['EFFECT'].str.contains('synon')]='Synon'
         f_use['Type'][f_use['EFFECT'].str.contains('frameshift')]='TRUNC'
         f_use['Type'][f_use['EFFECT'].str.contains('stop_gain')]='TRUNC'
         f_use['Type'][f_use['EFFECT'].str.contains('splice_donor')]='TRUNC'
@@ -61,6 +68,7 @@ if fileuse != []:
     fordl = convert_df_to_csv(all_fs[['Sample','ENTREZ.ID','Type']])
     entrezid = convert_df_to_csv(pd.read_csv('entrezids.txt',sep='\t', header=0, index_col=None))
     sample_names=all_fs[['Sample']].drop_duplicates()
+    st.write(all_fs)
     annotation_samples=convert_df_to_csv(pd.DataFrame({'Sample.ID':sample_names['Sample'],'Copy.Number':0, 'BCL2.transloc':'NA','BCL6.transloc':'NA'}))
     st.download_button("Download flat mutation file for lymphgen", fordl, "flatfilewmutations.txt", "text/csv", key='download-txt-mutants')
     st.download_button("Download entrez ids for lymphgen", entrezid, "entrezids.txt", "text/csv", key='download-txt-entrez')
